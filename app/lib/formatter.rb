1899175af7b7310414b2ce8c2c8a1b7100d18260
Unknown <admin@witches.live>
2018-10-12 11:35:19 -0700

    Revert "update to 2.5.2"
    This reverts commit 580262f6ba0a63ef842dc2f6a7d71a6d36446cbd.

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -90,12 +90,8 @@ class Formatter
 
   private
 
-  def html_entities
-    @html_entities ||= HTMLEntities.new
-  end
-
   def encode(html)
-    html_entities.encode(html)
+    HTMLEntities.new.encode(html)
   end
 
   def encode_and_link_urls(html, accounts = nil, options = {})
@@ -147,7 +143,7 @@ class Formatter
         emoji     = emoji_map[shortcode]
 
         if emoji
-          replacement = "<img draggable=\"false\" class=\"emojione\" alt=\":#{encode(shortcode)}:\" title=\":#{encode(shortcode)}:\" src=\"#{encode(emoji)}\" />"
+          replacement = "<img draggable=\"false\" class=\"emojione\" alt=\":#{shortcode}:\" title=\":#{shortcode}:\" src=\"#{emoji}\" />"
           before_html = shortname_start_index.positive? ? html[0..shortname_start_index - 1] : ''
           html        = before_html + replacement + html[i + 1..-1]
           i          += replacement.size - (shortcode.size + 2) - 1
@@ -216,7 +212,7 @@ class Formatter
     return link_to_account(acct) unless linkable_accounts
 
     account = linkable_accounts.find { |item| TagManager.instance.same_acct?(item.acct, acct) }
-    account ? mention_html(account) : "@#{encode(acct)}"
+    account ? mention_html(account) : "@#{acct}"
   end
 
   def link_to_account(acct)
@@ -225,7 +221,7 @@ class Formatter
     domain  = nil if TagManager.instance.local_domain?(domain)
     account = EntityCache.instance.mention(username, domain)
 
-    account ? mention_html(account) : "@#{encode(acct)}"
+    account ? mention_html(account) : "@#{acct}"
   end
 
   def link_to_hashtag(entity)
@@ -243,10 +239,10 @@ class Formatter
   end
 
   def hashtag_html(tag)
-    "<a href=\"#{encode(tag_url(tag.downcase))}\" class=\"mention hashtag\" rel=\"tag\">#<span>#{encode(tag)}</span></a>"
+    "<a href=\"#{tag_url(tag.downcase)}\" class=\"mention hashtag\" rel=\"tag\">#<span>#{tag}</span></a>"
   end
 
   def mention_html(account)
-    "<span class=\"h-card\"><a href=\"#{encode(TagManager.instance.url_for(account))}\" class=\"u-url mention\">@<span>#{encode(account.username)}</span></a></span>"
+    "<span class=\"h-card\"><a href=\"#{TagManager.instance.url_for(account)}\" class=\"u-url mention\">@<span>#{account.username}</span></a></span>"
   end
 end
