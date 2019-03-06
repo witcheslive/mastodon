74836d0eab1585aa154fa91aa3edf80c5b1d2a49
Unknown <admin@witches.live>
2019-03-06 14:40:51 -0800

    username auth test
    force option to be true to see where the problem is

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -8,7 +8,7 @@ Doorkeeper.configure do
   end
 
   resource_owner_from_credentials do |_routes|
-    if (Setting.allow_username_login)
+    if (true) #(Setting.allow_username_login)
       if request.params[:username].include?('@')
         user = User.find_by(email: request.params[:username])
       else
