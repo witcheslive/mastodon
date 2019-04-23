9d4bf41af13bea179ceb5feebeaa8be96952ec12
anna (eurekapyros) <anna@witches.live>
2019-04-23 15:34:35 -0700

    fix syntax error

--- app/lib/user_settings_decorator.rb
+++ app/lib/user_settings_decorator.rb
@@ -67,12 +67,13 @@ class UserSettingsDecorator
     boolean_cast_setting 'setting_system_font_ui'
   end
 
+  def auto_play_gif_preference
+    boolean_cast_setting 'setting_auto_play_gif'
+  end
+
   def show_formatting_preference
     boolean_cast_setting 'setting_show_formatting'
   end
-
-  def auto_play_gif_preference
-    boolean_cast_setting 'setting_'
 
   def display_media_preference
     settings['setting_display_media']
