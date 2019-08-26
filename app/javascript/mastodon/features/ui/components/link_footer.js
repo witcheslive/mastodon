de06dbf6b39bf470c2df043d66635a307259003d
anna (eurekapyros) <anna@witches.live>
2019-08-26 12:41:28 -0700

    update footer

--- app/javascript/mastodon/features/ui/components/link_footer.js
+++ app/javascript/mastodon/features/ui/components/link_footer.js
@@ -95,8 +95,11 @@ class LinkFooter extends React.PureComponent {
         <p>
           <FormattedMessage
             id='getting_started.open_source_notice'
-            defaultMessage='Mastodon is open source software. You can contribute or report issues on GitHub at {github}.'
-            values={{ github: <span><a href={source_url} rel='noopener' target='_blank'>{repository}</a> (v{version})</span> }}
+            defaultMessage='Mastodon is AGPL-licenced libre software. You can contribute or report issues on GitHub at {github}. BTW, witches.live runs on Gentoo, and our witchy fork is at {witchesrepo}.'
+            values={{
+              github: <span><a href={source_url} rel='noopener' target='_blank'>{repository}</a> (v{version})</span>,
+              witchesrepo: <span><a href='https://github.com/witcheslive/mastodon' rel='noopener' target='_blank'>witcheslive/mastodon</a></span>
+            }}
           />
         </p>
       </div>
